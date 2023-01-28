8da9dab839233748749830c4bb8bfeba48b72d2c
Hagar Fisher <hagarfisher@gmail.com>
2023-01-28 22:07:05 +0200

    adding functionality to drawer in header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -10,33 +10,71 @@ import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
+import { Drawer } from "@mui/material";
+import useDeviceDetect from "../../hooks/useDeviceDetect";
+import DrawerMenu from "./DrawerMenu";
+
+const drawerWidth = 240;
 
 export default function Header() {
+  const { isMobile } = useDeviceDetect();
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
+          {!isMobile && (
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              onClick={handleDrawerToggle}
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+          )}
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Recipes Tracker
           </Typography>
-          {navLinks.map((link, index) => {
-            return (
-              <Button color="inherit" key={link.path}>
-                <Link href={link.path}>{link.name}</Link>
-              </Button>
-            );
-          })}
+
+          {!isMobile &&
+            navLinks.map((link, index) => {
+              return (
+                <Button color="inherit" key={link.path}>
+                  <Link href={link.path}>{link.name}</Link>
+                </Button>
+              );
+            })}
         </Toolbar>
       </AppBar>
+      <Box component="nav">
+        <Drawer
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            // display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+          }}
+        >
+          <DrawerMenu
+            navLinks={navLinks}
+            handleDrawerToggle={handleDrawerToggle}
+          />
+        </Drawer>
+      </Box>
     </Box>
   );
 }
