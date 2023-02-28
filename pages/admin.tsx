74265e2b2b5d99694dea3f289b069b99755a33ae
Hagar Fisher <hagarfisher@gmail.com>
2023-02-28 22:38:39 +0200

    removing meal editing import

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -1,17 +1,10 @@
 import { useSession } from "@supabase/auth-helpers-react";
-import MealEditing from "../src/screens/MealEditing/MealEditing";
 import styles from "../styles/pages.module.scss";
 
 const Admin = () => {
   const session = useSession();
 
-  return (
-    session && (
-      <div className={styles.container}>
-        <MealEditing session={session} />
-      </div>
-    )
-  );
+  return session && <div className={styles.container}></div>;
 };
 
 export default Admin;
