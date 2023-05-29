f039a9d4a1882f96de3614f67077d9c5b7901dff
Hagar Fisher <hagarfisher@gmail.com>
2023-05-29 19:11:20 +0300

    adding style to auth component

--- src/components/Authentication/Auth.tsx
+++ src/components/Authentication/Auth.tsx
@@ -1,14 +1,19 @@
-import { Button, TextField } from "@mui/material";
+import styles from "./Auth.module.scss";
+import { Button, TextField, InputAdornment, Typography } from "@mui/material";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import LockIcon from "@mui/icons-material/Lock";
+import EmailIcon from "@mui/icons-material/Email";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 import React, { useContext, useEffect, useState } from "react";
 
-import { ID } from "appwrite";
+import { ID, Models } from "appwrite";
 import { AppWriteClientContext } from "../../contexts/AppWriteClientContext/AppWriteClientContext";
 
 export default function SignUp() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [isSignIn, setIsSignIn] = useState(false);
-  const { account } = useContext(AppWriteClientContext);
+  const { setSession, account } = useContext(AppWriteClientContext);
 
   if (!account) {
     return null;
@@ -16,40 +21,85 @@ export default function SignUp() {
 
   const handleSubmit = async () => {
     if (isSignIn) {
-      await account.createEmailSession(email, password);
+      const session = await account.createEmailSession(email, password);
+      setSession(session);
     } else {
-      await account.create(ID.unique(), email, password);
+      const user = await account.create(ID.unique(), email, password);
     }
   };
 
-  return (
+  const signUpText = (
     <div>
-      <TextField
-        id="email"
-        label="Email"
-        variant="outlined"
-        onChange={(e) => setEmail(e.target.value)}
-        value={email}
-      />
-      <TextField
-        id="password"
-        label="Password"
-        type="password"
-        autoComplete="current-password"
-        onChange={(e) => setPassword(e.target.value)}
-        value={password}
-      />
-      <Button variant="contained" onClick={() => handleSubmit()}>
-        {isSignIn ? "Log in" : "Sign up"}
-      </Button>
-      {isSignIn ? (
-        <div>Don&apos;t have an account?</div>
-      ) : (
-        <div>Already have an account?</div>
-      )}
-      <span onClick={() => setIsSignIn(!isSignIn)}>
-        {isSignIn ? "Sign in" : "Sign Up"}
+      Don&apos;t have an account? &nbsp;
+      <span
+        className={styles["toggle-link"]}
+        onClick={() => setIsSignIn(!isSignIn)}
+      >
+        Sign Up
       </span>
+    </div>
+  );
+  const signInText = (
+    <div>
+      Already have an account? &nbsp;
+      <span
+        className={styles["toggle-link"]}
+        onClick={() => setIsSignIn(!isSignIn)}
+      >
+        Sign In
+      </span>
+    </div>
+  );
+  return (
+    <div className={styles["container"]}>
+      <Typography variant="h4" component="h1">
+        {isSignIn ? "Login" : "Create Account"}
+      </Typography>
+      <div className={styles["auth-container"]}>
+        <div className={styles["form-fields"]}>
+          <TextField
+            id="email"
+            placeholder="Email"
+            variant="standard"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <EmailIcon />
+                </InputAdornment>
+              ),
+            }}
+          />
+          <TextField
+            id="password"
+            placeholder="Password"
+            type="password"
+            variant="standard"
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <LockIcon />
+                </InputAdornment>
+              ),
+            }}
+            // autoComplete="current-password"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+          />
+        </div>
+        <div className={styles["auth-button"]}>
+          <Button
+            // className={styles["button"]}
+            variant="contained"
+            onClick={() => handleSubmit()}
+          >
+            {isSignIn ? "Log in" : "Sign up"}
+            <ArrowForwardIcon className={styles.icon} fontSize="small" />
+          </Button>
+        </div>
+        {isSignIn ? signUpText : signInText}
+      </div>
     </div>
   );
 }
