640f14f48e10a4c3e24a74a0b76f9e43286c50ec
Hagar Fisher <hagarfisher@gmail.com>
2023-05-29 13:49:06 +0300

    adding session and setSession

--- src/contexts/AppWriteClientContext/AppWriteClientContext.tsx
+++ src/contexts/AppWriteClientContext/AppWriteClientContext.tsx
@@ -1,6 +1,8 @@
-import { Account, Client } from "appwrite";
+import { Account, Client, Models } from "appwrite";
 import { createContext } from "react";
 export const AppWriteClientContext = createContext<{
   account: Account | null;
   client: Client | null;
-}>({ account: null, client: null });
+  session: Models.Session | null;
+  setSession: React.Dispatch<React.SetStateAction<Models.Session | null>>;
+}>({ account: null, client: null, session: null, setSession: () => {} });
